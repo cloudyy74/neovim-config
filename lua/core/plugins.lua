ecd46ba8a90b78ecdb592b91c8b1d7323704a7ed
Vitas <ccloudyy74@gmail.com>
2024-12-10 18:49:33 +0300

    again switched to tree-sitter

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -93,7 +93,7 @@ require("lazy").setup({
      lazy = false,
      init = function()
          vim.g.tex_flavor = "latex"
-         vim.g.vimtex_view_method = "mupdf"
+         vim.g.vimtex_view_general_viewer = "okular"
      end
 
 }, {'lyokha/vim-xkbswitch'}, {'talha-akram/noctis.nvim'}, {'sainnhe/sonokai'},
@@ -128,140 +128,17 @@ require("lazy").setup({
         dap.listeners.before.event_exited["dapui_config"] = function()
             dapui.close()
         end
-    end
+   end
 },
 {
     "nvim-neotest/nvim-nio"
-}, {
-    'sheerun/vim-polyglot',
-    lazy = false,
 },
+-- {
+--     'sheerun/vim-polyglot',
+--     lazy = false,
+-- },
 {
-    "scalameta/nvim-metals",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      {
-        "j-hui/fidget.nvim",
-        opts = {},
-      },
-      {
-        "mfussenegger/nvim-dap",
-        config = function(self, opts)
-          local dap = require("dap")
-
-          dap.configurations.scala = {
-            {
-              type = "scala",
-              request = "launch",
-              name = "RunOrTest",
-              metals = {
-                runType = "runOrTestFile",
-                --args = { "firstArg", "secondArg", "thirdArg" }, -- here just as an example
-              },
-            },
-            {
-              type = "scala",
-              request = "launch",
-              name = "Test Target",
-              metals = {
-                runType = "testTarget",
-              },
-            },
-          }
-        end
-      },
-    },
-    ft = { "scala", "sbt", "java" },
-    opts = function()
-      local metals_config = require("metals").bare_config()
-
-      metals_config.settings = {
-        showImplicitArguments = true,
-        excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
-      }
-
-      metals_config.init_options.statusBarProvider = "off"
-
-      metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-      metals_config.on_attach = function(client, bufnr)
-        require("metals").setup_dap()
-        map("n", "gD", vim.lsp.buf.definition)
-        map("n", "K", vim.lsp.buf.hover)
-        map("n", "gi", vim.lsp.buf.implementation)
-        map("n", "gr", vim.lsp.buf.references)
-        map("n", "gds", vim.lsp.buf.document_symbol)
-        map("n", "gws", vim.lsp.buf.workspace_symbol)
-        map("n", "<leader>cl", vim.lsp.codelens.run)
-        map("n", "<leader>sh", vim.lsp.buf.signature_help)
-        map("n", "<leader>rn", vim.lsp.buf.rename)
-        map("n", "<leader>f", vim.lsp.buf.format)
-        map("n", "<leader>ca", vim.lsp.buf.code_action)
-
-        map("n", "<leader>ws", function()
-          require("metals").hover_worksheet()
-        end)
-
-        map("n", "<leader>aa", vim.diagnostic.setqflist)
-
-        map("n", "<leader>ae", function()
-          vim.diagnostic.setqflist({ severity = "E" })
-        end)
-
-        map("n", "<leader>aw", function()
-          vim.diagnostic.setqflist({ severity = "W" })
-        end)
-
-        map("n", "<leader>d", vim.diagnostic.setloclist)
-
-        map("n", "[c", function()
-          vim.diagnostic.goto_prev({ wrap = false })
-        end)
-
-        map("n", "]c", function()
-          vim.diagnostic.goto_next({ wrap = false })
-        end)
-
-        map("n", "<leader>dc", function()
-          require("dap").continue()
-        end)
-
-        map("n", "<leader>dr", function()
-          require("dap").repl.toggle()
-        end)
-
-        map("n", "<leader>dK", function()
-          require("dap.ui.widgets").hover()
-        end)
-
-        map("n", "<leader>dt", function()
-          require("dap").toggle_breakpoint()
-        end)
-
-        map("n", "<leader>dso", function()
-          require("dap").step_over()
-        end)
-
-        map("n", "<leader>dsi", function()
-          require("dap").step_into()
-        end)
-
-        map("n", "<leader>dl", function()
-          require("dap").run_last()
-        end)
-      end
-
-      return metals_config
-    end,
-    config = function(self, metals_config)
-      local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = self.ft,
-        callback = function()
-          require("metals").initialize_or_attach(metals_config)
-        end,
-        group = nvim_metals_group,
-      })
-    end
+    "nvim-treesitter/nvim-treesitter", 
+    build = ":TSUpdate"
 }
 });
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,11 +1,14 @@
--- require'nvim-treesitter.configs'.setup {
---   ensure_installed = { "typescript", "make", "java", "kotlin", "sql",
---                        "lua", "haskell", "html", "css",  
---                        "go",  "python", "cpp", "c", "dockerfile", "latex",
---                        "tsx", "javascript", "rust" },
---   sync_install = false,
---   auto_install = true,
---   highlight = {
---     enable = true,
---   },
--- }
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = { "typescript", "make", "java", "kotlin", "sql",
+                       "lua", "haskell", "html", "css",  
+                       "go",  "python", "cpp", "c", "dockerfile", "latex",
+                       "tsx", "javascript", "rust", "scala" },
+  sync_install = false,
+  auto_install = true,
+  highlight = {
+    enable = true,
+  },
+  indent = {
+    enable = true,
+  }
+}
